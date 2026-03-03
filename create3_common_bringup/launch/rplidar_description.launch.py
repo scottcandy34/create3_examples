fe6ad12cc48bf0682e4f9c98574f22de7fd2f0ac
Scottcandy34 <bzbrandonz@gmail.com>
2026-03-03 16:12:24 -0500

    Groups RPLidar description launch nodes
    Encapsulates the robot state publisher and static transform publisher for the RPLidar description within a GroupAction. This improves the launch file's organization and prepares for potential future application of common actions to these nodes.
    
    - Fixes lidar placement issue

--- create3_common_bringup/launch/rplidar_description.launch.py
+++ create3_common_bringup/launch/rplidar_description.launch.py
@@ -4,9 +4,9 @@
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
+from launch.actions import DeclareLaunchArgument, GroupAction
 from launch.substitutions import Command, LaunchConfiguration, PathJoinSubstitution
-from launch_ros.actions import Node
+from launch_ros.actions import Node, PushRosNamespace
 
 
 ARGUMENTS = [
@@ -29,54 +29,56 @@ def generate_launch_description():
     visualize_rays = LaunchConfiguration('visualize_rays')
     namespace = LaunchConfiguration('namespace')
 
-    state_publisher = Node(
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
-        name='rplidar_state_publisher',
-        output='screen',
-        parameters=[
-            {'use_sim_time': False},
-            {'robot_description':
-             Command(
-                ['xacro', ' ', xacro_file, ' ',
-                 'namespace:=', namespace, ' ',
-                 'visualize_rays:=', visualize_rays])},
-            {'publish_frequency': 0.0},  # No TF publishing needed
-            {'frame_prefix': ''},  # No TF prefix
-        ],
-        remappings=[
-            ('robot_description', 'rplidar_description'),
-            ('/tf', 'tf'),
-            ('/tf_static', 'tf_static')
-        ],
-    )
-
-    tf_base_link_mount_link_publisher = Node(
-        package='tf2_ros',
-        executable='static_transform_publisher',
-        name='tf_base_link_mount_link_publisher',
-        output='screen',
-        arguments=[
-            '--x', '-0.015',
-            '--y', '0',
-            '--z', '0.1458',
-            '--yaw', '0',
-            '--pitch', '0',
-            '--roll', '0',
-            '--frame-id', 'base_link',
-            '--child-frame-id', 'mount_link'
-        ],
-        remappings=[
-            ('/tf', 'tf'),
-            ('/tf_static', 'tf_static')
-        ],
+    rplidar_group = GroupAction(
+        actions=[
+            Node(
+                package='robot_state_publisher',
+                executable='robot_state_publisher',
+                name='rplidar_state_publisher',
+                output='screen',
+                parameters=[
+                    {'use_sim_time': False},
+                    {'robot_description':
+                    Command(
+                        ['xacro', ' ', xacro_file, ' ',
+                        'namespace:=', namespace, ' ',
+                        'visualize_rays:=', visualize_rays])},
+                    {'publish_frequency': 0.0},  # No TF publishing needed
+                    {'frame_prefix': ''},  # No TF prefix
+                ],
+                remappings=[
+                    ('robot_description', 'rplidar_description'),
+                    ('/tf', 'tf'),
+                    ('/tf_static', 'tf_static')
+                ],
+            ),
+            Node(
+                package='tf2_ros',
+                executable='static_transform_publisher',
+                name='tf_base_link_mount_link_publisher',
+                output='screen',
+                arguments=[
+                    '--x', '-0.015',
+                    '--y', '0',
+                    '--z', '0.1458',
+                    '--yaw', '0',
+                    '--pitch', '0',
+                    '--roll', '0',
+                    '--frame-id', 'base_link',
+                    '--child-frame-id', 'mount_link'
+                ],
+                remappings=[
+                    ('/tf', 'tf'),
+                    ('/tf_static', 'tf_static')
+                ],
+            )
+        ]
     )
 
     # Define LaunchDescription variable
     ld = LaunchDescription(ARGUMENTS)
 
     # Add nodes to LaunchDescription
-    ld.add_action(state_publisher)
-    ld.add_action(tf_base_link_mount_link_publisher)
+    ld.add_action(rplidar_group)
 
     return ld
